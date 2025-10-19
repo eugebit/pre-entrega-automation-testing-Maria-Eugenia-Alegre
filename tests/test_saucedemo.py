bb29e748955ed20044337d58d86ecc9d78852a39
Eugenia Alegre <eugenia.alegre@gmail.com>
2025-10-19 00:30:55 -0300

    Se agrega la carpeta evidencias para guardar los screenshots, un metodo para guardar screenshots y las chrome_options para que no muestre el cartel de la contraseña

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -1,11 +1,7 @@
 import time
 import pytest
 import selenium
-
-
-
-
-from utils.helpers import login_saucedemo, get_driver
+from utils.helpers import login_saucedemo, get_driver, take_screenshot
 from selenium.webdriver.common.by import By
 import sys
 import os
@@ -20,26 +16,27 @@ def driver():
     driver.quit()
 
 
-# def test_login(driver):
-#     login_saucedemo(driver)
-#     time.sleep(4)
-#     path = '../Evidencias/test_login.png'
-#     driver.save_screenshot(path)
-#     assert '/inventory.html' in driver.current_url
-#     # por medio de Css_selector,dentro de este div padre va a ir a buscar una clase llamada título
-#     title = driver.find_element(By.CSS_SELECTOR, 'div.header_secondary_container .title').text
-#     assert title == 'Products'
+def test_login(driver):
+    test_name = 'test_login'
+    login_saucedemo(driver)
+    time.sleep(4)
+    take_screenshot(driver, test_name)
+    assert '/inventory.html' in driver.current_url
+    # por medio de Css_selector,dentro de este div padre va a ir a buscar una clase llamada título
+    title = driver.find_element(By.CSS_SELECTOR, 'div.header_secondary_container .title').text
+    assert title == 'Products'
 
 
 def test_catalog(driver):
+    test_name = 'test_catalog'
     login_saucedemo(driver)
     time.sleep(2)
     products = driver.find_elements(By.CLASS_NAME, 'inventory_item')
     time.sleep(2)
     title = driver.find_element(By.CLASS_NAME, 'app_logo').text
     menu = driver.find_element(By.ID, "react-burger-menu-btn").text
-    path = '../Evidencias/test_catalog.png'
-    driver.save_screenshot(path)
+    take_screenshot(driver, test_name)
+
     # Verifico que el titulo este correcto
     assert title == 'Swag Labs'
     # verifico que haya una lista de productos con al menos un producto
@@ -48,6 +45,7 @@ def test_catalog(driver):
 
 
 def test_shopping_cart(driver):
+    test_name = 'test_shopping_cart'
     login_saucedemo(driver)
     time.sleep(4)
 
@@ -55,11 +53,9 @@ def test_shopping_cart(driver):
     products[0].find_element(By.TAG_NAME, 'button').click()
     time.sleep(1)
     cant_items_carts = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge').text
-
     driver.find_element(By.ID, 'shopping_cart_container').click()
     time.sleep(2)
     title = driver.find_element(By.CLASS_NAME, 'title').text
-    path = '../Evidencias/test_shopping_cart.png'
-    driver.save_screenshot(path)
+    take_screenshot(driver, test_name)
     assert cant_items_carts == '1'
     assert title == 'Your Cart'
--- utils/helpers.py
+++ utils/helpers.py
@@ -14,25 +14,41 @@ _INPUT_NAME = 'user-name'
 _INPUT_PASSWORD = 'password'
 _LOGIN_BUTTON = 'login-button'
 
+
 def get_driver():
+    """
+    --password-store=basic: This argument sets the password storage backend to a basic,
+    non-integrated option, which effectively prevents Chrome's built-in password manager
+    from functioning.
+    no sé porque no está funcionando
+    """
     chrome_options = Options()
+    chrome_options.add_argument("--guest")
     chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-notifications')
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument('--start-maximized')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    service=Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
-
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
     time.sleep(5)
     return driver
 
+
 def login_saucedemo(driver):
     driver.get(URL)
     time.sleep(2)
-    #ingreso las credenciales
-    driver.find_element(By.NAME,_INPUT_NAME).send_keys(USERNAME)
+    # ingreso las credenciales
+    driver.find_element(By.NAME, _INPUT_NAME).send_keys(USERNAME)
     time.sleep(2)
-    driver.find_element(By.NAME,_INPUT_PASSWORD).send_keys(PASSWORD)
+    driver.find_element(By.NAME, _INPUT_PASSWORD).send_keys(PASSWORD)
     time.sleep(2)
-    driver.find_element(By.NAME,_LOGIN_BUTTON).click()
+    driver.find_element(By.NAME, _LOGIN_BUTTON).click()
     time.sleep(2)
+
+
+def take_screenshot(driver, name):
+    path = '../Evidencias/' + name + '.png'
+    driver.save_screenshot(path)
